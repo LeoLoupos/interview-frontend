8a5e1bdf76a851cc3965db82bc603a174cc36889
LeoLoupos <leo.loupos@gmail.com>
2018-06-06 17:27:29 +0300

    v0.3
    v0.3 : Adding Upkey functionality and Html view for the search bar

--- src/app/articles-search/articles-search.component.ts
+++ src/app/articles-search/articles-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-articles-search',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles-search.component.css']
 })
 export class ArticlesSearchComponent implements OnInit {
+  articles: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  async onKey(event: any) { // without type info
+    await this.http.get(`http://localhost:3000/api/articles/searchTitle?title=${event.target.value.trim()}`).subscribe( (data: any) => {
+      //we pass to the articles , the data.orders
+      this.articles = data.articles.map(value => {
+
+        if (value) {
+          //Transforming the timestamp to DateString , and then we split to only the first space.
+          //We also cast to interget using a typescript technique , the + operator sticked to the variable.
+          let createdAt = new Date(+value.createdat).toDateString();
+
+          //Date format : MMM DD YYYY
+          createdAt = createdAt.substr(createdAt.indexOf(' ') + 1);
+
+          //for each mapped value, we return the object we are seeking
+          return {
+            article_id: value.article_id,
+            title : value.title,
+            thumbnail: value.thumbnail,
+            creator : {
+              name: value.creator.name,
+              profileUrl: value.creator.profileurl
+            },
+            createdAt: createdAt
+          };
+        } else {
+          this.articles = [];
+        }
+    });
+
+    }, err => {
+      if (err.status === 500) {
+
+      } else {
+
+      }
+    });
   }
 
 }
